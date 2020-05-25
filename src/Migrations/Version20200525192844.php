38368e995e5162eb6bdf6ae71aa5a44d87e921d9
jlorenzop <jlorenzo681@gmail.com>
2020-05-25 22:48:30 +0200

    migraciones

---
+++ src/Migrations/Version20200525192844.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200525192844 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ciudad ADD norte INT NOT NULL, ADD sur INT NOT NULL, ADD este INT NOT NULL, ADD oeste INT NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ciudad DROP norte, DROP sur, DROP este, DROP oeste');
+    }
+}
---
+++ src/Migrations/Version20200525203116.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200525203116 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ciudad CHANGE norte norte DOUBLE PRECISION NOT NULL, CHANGE sur sur DOUBLE PRECISION NOT NULL, CHANGE este este DOUBLE PRECISION NOT NULL, CHANGE oeste oeste DOUBLE PRECISION NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ciudad CHANGE norte norte INT NOT NULL, CHANGE sur sur INT NOT NULL, CHANGE este este INT NOT NULL, CHANGE oeste oeste INT NOT NULL');
+    }
+}
